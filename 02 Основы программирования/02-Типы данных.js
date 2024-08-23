225977adee2d47d2e63b16608ab42fd612d2056e
UMAtaullin <ataullinural@gmail.com>
2024-08-23 13:52:40 +0500

    Renamed according to course

---
+++ 02 Основы программирования/02-Типы данных.js
@@ -0,0 +1,22 @@
+// Два простых типа
+undefined
+null
+
+// Boolean
+true
+false
+
+// String
+('Hello world')
+"Hi there" `Hello again`
+
+// Number
+18
+3.14
+NaN
+
+// Object
+// объект
+// массив
+// функции
+// ошибка
