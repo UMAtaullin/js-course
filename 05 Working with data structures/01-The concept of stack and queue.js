67aa26b68a1fdd697fd927f2ec948be747daaa00
UMAtaullin <ataullinural@gmail.com>
2024-09-01 11:25:27 +0500

    FIFO & LIFO

---
+++ 05 Working with data structures/01-The concept of stack and queue.js
@@ -0,0 +1,28 @@
+// FIFO, LIFO
+
+// Queue - first in, first out
+// push/shift
+const queue = []
+queue.push(4)
+queue.push(7)
+queue.push(9)
+
+console.log(queue)
+
+queue.shift()
+console.log(queue)
+
+// Stack - first in, last out
+// push/pop
+const stack = []
+stack.push(5)
+stack.push(3)
+stack.push(2)
+stack.push(1)
+
+console.log(stack)
+
+stack.pop()
+stack.pop()
+
+console.log(stack)
