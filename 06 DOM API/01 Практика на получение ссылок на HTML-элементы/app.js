e8f34af721f6996e0fb3cd8eba2b2668b465e7c1
UMAtaullin <ataullinural@gmail.com>
2024-09-10 06:51:33 +0500

    feat: getting links to HTML elements

--- 06 DOM API/01 Практика на получение ссылок на HTML-элементы/app.js
+++ 06 DOM API/01 Практика на получение ссылок на HTML-элементы/app.js
@@ -5,3 +5,11 @@ const btn = document.getElementsByTagName('button')[0];
 const subtitle = document.querySelector('h2.gray');
 
 const sections = document.getElementsByTagName('section');
+
+// const title = document.querySelector('h1')
+
+// const btn = document.querySelector('#btn')
+
+// const subtitle = document.querySelector('.title.gray')
+
+// const sections = document.querySelectorAll('section')
