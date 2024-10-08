632639cd1f7d494b8698f67c76b18c44c1eae322
UMAtaullin <ataullinural@gmail.com>
2024-10-08 06:47:18 +0500

    using event key and event target

---
+++ 06 DOM API/17-Modal window/app.js
@@ -0,0 +1,40 @@
+const btn = document.getElementById('btnId')
+const modal = document.getElementById('modalId')
+const close = document.querySelector('.close')
+
+btn.addEventListener('click', openModal)
+close.addEventListener('click', closeModal)
+
+function openModal() {
+  modal.classList.add('open')
+  attachModalEvents()
+}
+
+function closeModal() {
+  modal.classList.remove('open')
+  detachModalEvents()
+}
+// Close modal on outside click
+function outsideClick(event) {
+  if (event.target.closest('.modal') === modal) {
+    closeModal()
+  }
+}
+// Close modal on escape key press
+function handleEscape(event) {
+  if (event.key === 'Escape') {
+    closeModal()
+  }
+}
+
+function attachModalEvents() {
+  close.addEventListener('click', closeModal);
+  document.addEventListener('click', outsideClick);
+  document.addEventListener('keydown', handleEscape);
+}
+
+function detachModalEvents() {
+  close.removeEventListener('click', closeModal);
+  document.removeEventListener('click', outsideClick);
+  document.removeEventListener('keydown', handleEscape);
+}
