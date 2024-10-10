ce38d0daa459c0342c17fd4831d5ad09996c74fb
UMAtaullin <ataullinural@gmail.com>
2024-10-10 07:35:36 +0500

    using this getAttribute and setAttribute

--- 06 DOM API/11-Event object/practice/app.js
+++ 06 DOM API/11-Event object/practice/app.js
@@ -1,1 +1,10 @@
+buttons = document.querySelectorAll('[data-clicked]')
 
+buttons.forEach(el => el.addEventListener('click', clickCounter))
+
+function clickCounter () {
+  // console.log(this.dataset.clicked++)
+  let count = this.getAttribute('data-clicked') || 0
+  this.setAttribute('data-clicked', ++count)
+  console.log(`Clicked ${count} times`)
+}
