57507b1014d4fee08ee6ab8634b62bcedf8b4ab1
UMAtaullin <ataullinural@gmail.com>
2024-10-13 07:37:44 +0500

    delete previous files

--- 06 DOM API/02 Практика на работу с событием click/app.js
+++
@@ -1,7 +0,0 @@
-let buttons = document.querySelectorAll('.accordion')
-
-buttons.forEach(el => el.addEventListener('click', changeVisible))
-
-function changeVisible () {
-    this.nextElementSibling.classList.toggle('show')
-}
